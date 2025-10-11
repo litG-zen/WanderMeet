15c1b63df02a4462aa986b39277ea36183c2794b
litG-zen <lalit.coder2296@gmail.com>
2025-10-11 22:49:20 +0530

    Commit 1: Redis connector addition

--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.24.4
 
 require (
 	github.com/gin-gonic/gin v1.11.0
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang-jwt/jwt/v5 v5.3.0
 )
 
 require (
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
---
+++ utils/redisConnector.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var (
+	redisClient *RedisClient
+	redisOnce   sync.Once
+	REDIS_URL   = os.Getenv("REDIS_URL")
+)
+
+type RedisClient struct {
+	client *redis.Client
+	ctx    context.Context
+	mutex  sync.RWMutex
+}
+
+func (r *RedisClient) initialize() error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	fmt.Printf("REDIS_URL :%v", REDIS_URL)
+	if REDIS_URL == "" {
+		return fmt.Errorf("REDIS URL is not defined")
+	}
+
+	opt, err := redis.ParseURL(REDIS_URL)
+	if err != nil {
+		return fmt.Errorf("Failed to parse REDIS URL : %v", REDIS_URL)
+
+	}
+
+	r.client = redis.NewClient(opt)
+
+	// Test connection
+	_, err = r.client.Ping(r.ctx).Result()
+	if err != nil {
+		return fmt.Errorf("failed to connect to redis instance %v", err)
+	}
+
+	return nil
+}
+
+func NewRedisClient() (*RedisClient, error) {
+	var initErr error
+	redisOnce.Do(func() {
+		redisClient = &RedisClient{ctx: context.Background()}
+		initErr = redisClient.initialize()
+	})
+
+	if initErr != nil {
+		return nil, initErr
+	}
+	return redisClient, nil
+}
+
+func GetRedisInstance() (*RedisClient, error) {
+	if redisClient == nil {
+		return NewRedisClient()
+	}
+	return redisClient, nil
+}
